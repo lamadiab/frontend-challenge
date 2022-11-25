272d2984db2cb78ce262b07bc0b3b7e23f14ee77
Lama Diab <lama.diab@mischok.de>
2022-11-25 17:42:19 +0100

    add some clarifying comments

--- src/tests/e2e/domManipulationView.test.tsx
+++ src/tests/e2e/domManipulationView.test.tsx
@@ -28,7 +28,15 @@ describe('super awesome end to end tests', () => {
   //     await waitFor(() => screen.getByTestId('tooltipContainer'));
   //     expect(screen.getByTestId('tooltip')).toBeInTheDocument();
   //   });
+
+  /**
+   * I was trying to test the mouse over functionality. After some trials,
+   * I realized the script gets omitted from the tree during the test.
+   */
   it('renders correctly', () => {
+    /** I kept getting an error "palette of theme is undefined.
+    wrapping the component in theme provider seems (themes :p) to be a functional workaround.*/
+
     const theme = createTheme();
     const MockDomManipulationView = () => {
       return (
