f3e95453c940c07e80a87d977bbf7ae0ec486046
Lama Diab <lama.diab@mischok.de>
2022-11-25 17:30:44 +0100

    add user history table

--- src/views/user/UserHistoryView.tsx
+++ src/views/user/UserHistoryView.tsx
@@ -1,5 +1,18 @@
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { ReactElement } from 'react';
 
+import dummyData from '../../../dummyData.json';
+
 export const UserHistoryView = (): ReactElement => {
-  return <div>{'I could be a table'}</div>;
+  const tripHistory = dummyData.HISTORY_DATA;
+  const columns: GridColDef[] = [];
+
+  const rows = [];
+
+  let i = 0;
+
+  for (const entry in tripHistory) rows.push({ id: i++, ...tripHistory[entry] });
+  for (const key in tripHistory[0]) columns.push({ field: key, headerName: key.toUpperCase(), width: 180 });
+
+  return <div style={{ height: 400, width: '100%' }}>{<DataGrid rows={rows} columns={columns} />}</div>;
 };
